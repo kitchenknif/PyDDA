3a3de34e4bb3b520d011afbc4d65b289592f7f01
Pavel Dmitriev <pavel.a.dmitriev@gmail.com>
2015-08-31 16:13:39 +0300

    working on repeating Evlukhin's results.

--- examples/evlukhin-free-space/Si_sphere_scattering.py
+++ examples/evlukhin-free-space/Si_sphere_scattering.py
@@ -9,6 +9,7 @@ from dda_si_funcs import *
 from PyTMM import refractiveIndex
 import scatterer
 import plot_funcs
+import spec
 
 pow1d3 = power_function(1. / 3.)
 pow2 = power_function(2)
@@ -16,39 +17,26 @@ pow3 = power_function(3)
 
 points = 50
 # Spherical particle
-lambda_range = np.linspace(600, 820, points)  # nm
-diameter = 97*2 # nm
+lambda_range = np.linspace(400, 650, points)  # nm
+diameter = 63*2 # nm
 
 k = 2 * np.pi  # wave number
 
-r, N, d_old = scatterer.dipole_sphere(9, diameter)
+r, N, d_old = scatterer.dipole_sphere(11, diameter)
 plot_funcs.plot_dipoles(r)
-#r = misc.load_dipole_file('../shape/sphere_912.txt')
-#N = numpy.shape(r)[0]
-#d_old = 1
 
-catalog = refractiveIndex.RefractiveIndex("../../../../RefractiveIndex/")
+catalog = refractiveIndex.RefractiveIndex("../../../RefractiveIndex/")
 Si = catalog.getMaterial('main', 'Si', 'Vuye-20C')
 
 nSi = np.asarray([Si.getRefractiveIndex(lam) + Si.getExtinctionCoefficient(lam)*1j for lam in lambda_range])
 
-Cscat = np.zeros(lambda_range.size)
 Cext = np.zeros(lambda_range.size)
-Cabs = np.zeros(lambda_range.size)
-
 
 #
 # incident plane wave
 #
-
-#Incident field wave vector angle
-gamma_deg = 22.5
-gamma = gamma_deg / 180 * np.pi
 kvec = k * np.asarray([0, 0, 1])  # wave vector [x y z]
-
-#Incident field polarization
-E0 = np.asarray([1, 0, 0])
-
+E0 = np.asarray([1, 0, 0])  # Incident field polarization
 #
 #
 #
@@ -63,6 +51,7 @@ for ix, lam in enumerate(lambda_range):
     a_eff = diameter / (2 * lam)  # effective radius in wavelengths
 
     d_new = pow1d3(4 / 3 * np.pi / N) * a_eff
+    #d_new = a_eff / 10
     r *= (d_new / d_old)
     d_old = d_new
 
@@ -74,15 +63,18 @@ for ix, lam in enumerate(lambda_range):
     A = interaction_A(k, r, alph)
     P = scipy.sparse.linalg.gmres(A, Ei)[0]
 
-    Cext[ix] = C_ext(k, E0, Ei, P)
-    Cabs[ix] = C_abs(k, E0, Ei, P, alph)
-    Cscat[ix] = Cext[ix] - Cabs[ix]
+    Cext[ix] = C_ext(k, E0, Ei, P) * (lam/100)**2 / 100 #Convert to um**2
+
+evlukhin = spec.Spec.loadSpecFromASCII("./reference/sphere_r63nm.txt", ',')
+
+#Cext = numpy.divide(Cext, numpy.max(Cext))
+#evlukhin.data = numpy.divide(evlukhin.data, numpy.max(evlukhin.data))
 
 
 plt.figure(1)
-plt.plot(lambda_range, Cscat)
-plt.plot(lambda_range, Cabs)
 plt.plot(lambda_range, Cext)
-plt.legend(['Scat', 'Abs', 'Ext'])
-#title(['gamma = ' + str(gamma * 180 / pi) + ', zgap = ' + str(zgap) + ', N = ' + str(N)])
+plt.plot(evlukhin.wavelengths, evlukhin.data)
+plt.ylabel("\sigma_{ext}, \mu m^2")
+plt.xlabel("wavelength, nm")
+plt.legend(['PyDDA', 'Evlukhin'])
 plt.show(block=True)
